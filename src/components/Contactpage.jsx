265889eced7a30b5d0f713a2e639fe4014022105
Muhammad Hamza <125260259+muhammadh4mza@users.noreply.github.com>
2025-11-05 23:39:00 +0500

    Comment out phone number display in Contactpage
    Commented out the phone number section in Contactpage.

--- src/components/Contactpage.jsx
+++ src/components/Contactpage.jsx
@@ -58,7 +58,7 @@ const ContactPage = () => {
                 <div className="bg-blue-500 p-3 rounded-full mr-4">
                   <Phone className="text-white w-5 h-5" />
                 </div>
-                <div>
+                {/* <div>
                   <h4 className="text-lg font-semibold text-white">Phone</h4>
                   <a href="tel:0518481943" className="text-gray-300 hover:text-blue-400 transition">
                     (051) 848 1943
@@ -68,7 +68,7 @@ const ContactPage = () => {
                   <a href="tel:03155513155" className="text-gray-300 hover:text-blue-400 transition">
                     +92 315 551 3155
                   </a>
-                </div>
+                </div> */}
               </div>
 
               <div className="flex items-start">
